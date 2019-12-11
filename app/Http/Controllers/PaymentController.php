a93682c25669c687fb548f9c5d71068d8f200117
vanguard <championing@mail.ru>
2019-12-11 09:25:18 +0700

    Fixed an error

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -15,10 +15,6 @@ class PaymentController extends Controller
      */
     public function activate(Request $request)
     {
-        $data = $request->all();
-        $data = json_encode($data, JSON_PRETTY_PRINT);
-        file_put_contents('payment.txt', $data);
-
         if ($this->checkValidity($request)) {
             $list = GiftList::find($request->label);
             $list->activate();
