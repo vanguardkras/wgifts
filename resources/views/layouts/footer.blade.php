e4ddbb95870d0389bb99dd2f7c6570263096aeef
Max Shaian <championing@mail.ru>
2019-12-15 09:56:31 +0700

    Fixed telegram link

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div>
         <p>Email: <a href="mailto:championing@mail.ru">championing@mail.ru</a></p>
-        <p>Telegram: <a href="tg://resolve?domain=@vanguardkras">vanguardkras</a></p>
+        <p>Telegram: <a href="https://teleg.run/Anastasiya_Shayan">vanguardkras</a></p>
     </div>
     <div>
         <p>2019 &copy; SHAIAN</p>
